c10e91a671bf53c798fee3d9ef586909ae8c8b48
zl95826 <zl95826@hotmail.com>
2023-12-11 17:22:49 -0800

    add autocomplete box but cannot show selected value

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,14 @@
+"use client";
 import React from "react";
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      Search
+      <SearchManufacturer />
+    </div>
+  );
 };
 
 export default SearchBar;
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,53 @@
+import { Combobox, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
+import { manufacturers } from "@/constants";
+
+const SearchManufacturer = () => {
+  const [selected, setSelected] = useState("");
+  const [query, setQuery] = useState("");
+  const filteredManu =
+    query === ""
+      ? []
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+  return (
+    <div>
+      <Combobox value="" onChange={() => {}}>
+        <Combobox.Input
+          onChange={(event) => setQuery(event.target.value)}
+          placeholder="Volkswagen..."
+          displayValue={(item: string) => item}
+        />
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")}
+        >
+          <Combobox.Options>
+            {filteredManu.map((manu) => (
+              <Combobox.Option
+                key={manu}
+                value={manu}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                    active ? "bg-teal-600 text-white" : "text-gray-900"
+                  }`
+                }
+              >
+                {manu}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        </Transition>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
